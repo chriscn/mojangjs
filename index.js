14f0c9d9a8059a895eaf21517beebae37f27c93a
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-17 20:29:16 +0100

    Removed my testing

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 module.exports = require('./lib/api.js');
-
-const mojangjs = require('./lib/api.js');
-
-mojangjs.getUUID('Thorin', (err, user) => {
-	if (err) console.error(err);
-	mojangjs.getNameHistory(user.id, (err, namehistory) => {
-		if (err) console.error(err);
-		console.log(namehistory);
-	});
-});
