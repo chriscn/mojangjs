0a0902ab0cda3df08e912144c2134383e5ceb12e
Christopher Nethercott <ccnethercott@gmail.com>
2018-10-17 16:36:56 +0100

    Updated our name!

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./lib/mcuuid.js');
+module.exports = require('./lib/api.js');
+
+const mojangjs = require('./lib/api.js');
+
+mojangjs.getUUID('Thorin', (err, user) => {
+	if (err) console.error(err);
+	mojangjs.getNameHistory(user.id, (err, namehistory) => {
+		if (err) console.error(err);
+		console.log(namehistory);
+	});
+});
